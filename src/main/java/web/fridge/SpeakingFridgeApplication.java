70509cea013c9088588fefced3f5504f2b4e17ea
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-12-09 16:42:34 +0900

    🔧 fix: rename Application Name

--- src/main/java/web/fridge/SpeakingFridgeApplication.java
+++ src/main/java/web/fridge/SpeakingFridgeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpeakingFridgyApplication {
+public class SpeakingFridgeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpeakingFridgyApplication.class, args);
+		SpringApplication.run(SpeakingFridgeApplication.class, args);
 	}
 
 }
--- src/test/java/web/fridge/SpeakingFridgeApplicationTests.java
+++ src/test/java/web/fridge/SpeakingFridgeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpeakingFridgyApplicationTests {
+class SpeakingFridgeApplicationTests {
 
 	@Test
 	void contextLoads() {
