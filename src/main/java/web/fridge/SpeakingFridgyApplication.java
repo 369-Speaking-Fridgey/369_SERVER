efc62d247e0df18592e746b503fd306f9289bef9
sunnyineverywhere <sunnyinsummer@ewhain.net>
2022-12-09 16:41:19 +0900

    🎉 FIRST COMMIT

---
+++ src/main/java/web/fridge/SpeakingFridgyApplication.java
@@ -0,0 +1,13 @@
+package web.fridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakingFridgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakingFridgyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/fridge/SpeakingFridgyApplicationTests.java
@@ -0,0 +1,13 @@
+package web.fridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakingFridgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
