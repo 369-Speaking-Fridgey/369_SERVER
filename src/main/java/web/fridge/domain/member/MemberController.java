f72d7d554f5c8b2b234a908789c4e1fb89ec7b6b
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-01-11 11:14:28 +0900

    ✨feature: Add Security Config

---
+++ src/main/java/web/fridge/domain/member/MemberController.java
@@ -0,0 +1,13 @@
+package web.fridge.domain.member;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/members")
+public class MemberController {
+
+    @GetMapping("/test")
+    public String test(@RequestParam String req){
+        return req + "실행 완료";
+    }
+}
