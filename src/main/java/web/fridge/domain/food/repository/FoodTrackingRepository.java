6e40caf7bb598b6c9a7ca1b8159f46094ce2a75c
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-03-27 03:19:53 +0900

    ♻️ refactor: group by 적용해서 쿼리 조회 회수 줄임 #39

--- src/main/java/web/fridge/domain/food/repository/FoodTrackingRepository.java
+++ src/main/java/web/fridge/domain/food/repository/FoodTrackingRepository.java
@@ -1,17 +1,19 @@
 package web.fridge.domain.food.repository;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import web.fridge.domain.food.entity.FoodStatus;
-import web.fridge.domain.food.entity.QFood;
 import web.fridge.domain.member.entity.Member;
 
 import javax.persistence.EntityManager;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static web.fridge.domain.family.entity.QFamily.family;
 import static web.fridge.domain.food.entity.QFood.food;
@@ -23,9 +25,9 @@ public class FoodTrackingRepository {
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
 
-    public List<Long> countFoodGroupByStatusAtCurrentMonth(){
-        return queryFactory
-                .select(food.count())
+    public Map<FoodStatus, Long> countFoodGroupByStatusAtCurrentMonth(){
+        List<Tuple> queryResult = queryFactory
+                .select(food.status, food.count())
                 .from(food)
                 .leftJoin(fridge).on(food.fridge.fridgeId.eq(fridge.fridgeId))
                 .leftJoin(family).on(family.fridge.fridgeId.eq(fridge.fridgeId))
@@ -34,42 +36,29 @@ public class FoodTrackingRepository {
                 )
                 .groupBy(food.status)
                 .fetch();
+        Map<FoodStatus, Long> result = new HashMap<>();
+        for (Tuple tuple : queryResult) {
+            result.put(tuple.get(food.status), tuple.get(food.count()));
+        }
+        return result;
     }
 
-    public Long countFoodCreatedByMemberAtCurrentMonth(Member member){
-        return queryFactory
-                .select(food.count())
+    public Map<FoodStatus, Long> countFoodCreatedByMemberAtCurrentMonth(Member member){
+        List<Tuple> queryResult = queryFactory
+                .select(food.status, food.count())
                 .from(food)
                 .leftJoin(fridge).on(food.fridge.fridgeId.eq(fridge.fridgeId))
                 .leftJoin(family).on(family.fridge.fridgeId.eq(fridge.fridgeId))
                 .where(family.member.eq(member),
                         food.createdAt.eq(LocalDateTime.now().withMonth(LocalDate.now().getMonthValue()))
                 )
-                .fetchFirst();
-    }
-
-    public Long countFoodConsumedByMemberAtCurrentMonth(Member member){
-        return queryFactory
-                .select(food.count())
-                .from(food)
-                .leftJoin(fridge).on(food.fridge.fridgeId.eq(fridge.fridgeId))
-                .leftJoin(family).on(family.fridge.fridgeId.eq(fridge.fridgeId))
-                .where(family.member.eq(member),
-                        food.createdAt.eq(LocalDateTime.now().withMonth(LocalDate.now().getMonthValue())),
-                        food.status.eq(FoodStatus.CONSUMED)
-                        ).fetchFirst();
-    }
-
-    public Long countFoodWastedByMemberAtCurrentMonth(Member member){
-        return queryFactory
-                .select(food.count())
-                .from(food)
-                .leftJoin(fridge).on(food.fridge.fridgeId.eq(fridge.fridgeId))
-                .leftJoin(family).on(family.fridge.fridgeId.eq(fridge.fridgeId))
-                .where(family.member.eq(member),
-                        food.createdAt.eq(LocalDateTime.now().withMonth(LocalDate.now().getMonthValue())),
-                        food.status.eq(FoodStatus.WASTED)
-                ).fetchFirst();
+                .groupBy(food.status)
+                .fetch();
+        Map<FoodStatus, Long> result = new HashMap<>();
+        for (Tuple tuple : queryResult) {
+            result.put(tuple.get(food.status), tuple.get(food.count()));
+        }
+        return result;
     }
 
 }
