2b34ff2919f903f80c0517546ac27faab1de905a
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-05-16 11:49:00 +0900

    🔧 fix: fridgeResponseDTO에 getter 추가

--- src/main/java/web/fridge/domain/fridge/controller/dto/FridgeResponseDTO.java
+++ src/main/java/web/fridge/domain/fridge/controller/dto/FridgeResponseDTO.java
@@ -1,9 +1,11 @@
 package web.fridge.domain.fridge.controller.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 import web.fridge.domain.fridge.entity.Fridge;
 import web.fridge.domain.member.entity.Member;
 
+@Getter
 public class FridgeResponseDTO {
     private String email;
     private Long fridgeId;
